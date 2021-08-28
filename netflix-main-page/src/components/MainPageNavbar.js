b274d9d21c70b8e4dae0fc66c620eb200b1f1df5
peretarrida13 <pere.tarrida13@gmail.com>
2021-08-28 11:23:18 +0200

    final commit

--- netflix-main-page/src/components/MainPageNavbar.js
+++ netflix-main-page/src/components/MainPageNavbar.js
@@ -23,7 +23,7 @@ export const MainPageNavbar = ()=>{
     return(
         <nav className={showNavbar ? 'navbarMainPage-show' : 'navbarMainPage'}>
             <Link to='/mainpage'><img src={topMiniLogo} style={LogoBtn}/></Link>
-            <img src='https://occ-0-4671-360.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABUCZYfPbupvQjzSa3egePk8TFNDy2A_w15DEAq50IqW8MYmOtmbWwN4Txem7mgNYEMPJ1BY6uasiIJQ8JeFO3EU.png?r=b97' alt='profileImg' style={profileImgNavbar} onClick={changeProfile}/>
+            <img src='https://occ-0-4671-360.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABUCZYfPbupvQjzSa3egePk8TFNDy2A_w15DEAq50IqW8MYmOtmbWwN4Txem7mgNYEMPJ1BY6uasiIJQ8JeFO3EU.png?r=b97' alt='profileImg' style={profileImgNavbar}/>
         </nav>
     );
 }
